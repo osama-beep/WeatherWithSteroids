69ea53936f511aa1d5e29162931a414e1a7243d5
osama-beep <oennabati3@gmail.com>
2025-01-17 15:09:09 +0100

    commit 5

--- src/components/WeekWeather.jsx
+++ src/components/WeekWeather.jsx
@@ -1,35 +1,32 @@
 import PropTypes from "prop-types";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card, Row, Col, Container } from "react-bootstrap";
 
 function WeekWeather({ forecast, isDarkTheme }) {
   return (
-    <Row className="mt-4 justify-content-center">
-      {forecast.slice(0, 6).map((day, index) => (
-        <Col key={index} xs={7} sm={4} md={3} lg={2} className="mb-3 px-1">
-          <Card
-            className={`text-center ${
-              isDarkTheme ? "bg-secondary text-light" : "bg-light text-dark"
-            }`}
-          >
-            <Card.Body className="p-1">
-              <Card.Title className="mb-0 fs-6">
-                {new Date(day.date).toLocaleDateString("it-IT", {
-                  day: "numeric",
-                  month: "numeric",
-                })}
-              </Card.Title>
-              <img
-                src={`http://openweathermap.org/img/wn/${day.icon}.png`}
-                alt={day.description}
-                className="weather-icon"
-              />
-              <Card.Text className="mb-0">{day.temperature}°C</Card.Text>
-              <Card.Text className="small">{day.description}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Container>
+      <Row className="mt-4 justify-content-center">
+        {forecast.slice(1, 6).map((day, index) => (
+          <Col key={index} xs={7} sm={4} md={3} lg={2} className="mb-3 px-1">
+            <Card
+              className={`text-center ${
+                isDarkTheme ? "bg-secondary text-light" : "bg-light text-dark"
+              }`}
+            >
+              <Card.Body className="p-1">
+                <Card.Title className="mb-0 fs-6">{day.date}</Card.Title>
+                <img
+                  src={`http://openweathermap.org/img/wn/${day.icon}.png`}
+                  alt={day.description}
+                  className="weather-icon"
+                />
+                <Card.Text className="mb-0">{day.temperature}°C</Card.Text>
+                <Card.Text className="small">{day.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
